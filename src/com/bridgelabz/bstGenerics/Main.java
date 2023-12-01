72f05b5f4ecd9275fc843ec59cd3fd79768fa2b6
aDHIxx <sreemohan.adhithya@gmail.com>
2023-12-01 10:46:34 +0530

    Added: UseCase 2: Create BST using LinkedList according to the given tree

--- src/com/bridgelabz/bstGenerics/Main.java
+++ src/com/bridgelabz/bstGenerics/Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String[] args) {
         MyBinarySearchTree<Integer> bst = new MyBinarySearchTree<>();
 
-        // Adding nodes to the BST
         bst.add(56);
         bst.add(30);
+        bst.add(22);
+        bst.add(11);
+        bst.add(3);
+        bst.add(16);
+        bst.add(40);
         bst.add(70);
+        bst.add(60);
+        bst.add(95);
+        bst.add(65);
+        bst.add(63);
+        bst.add(67);
 
         // Displaying the in-order traversal of the BST
         System.out.println("In-order traversal of the BST:");
